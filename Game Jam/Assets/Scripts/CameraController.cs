b91d985d8aa089938877a5ef3c949a6126b3e88d
Martin Kovac <20kovmar@hawken.edu>
2020-02-27 14:38:27 -0500

    Messed Around with White Cell

---
+++ Game Jam/Assets/Scripts/CameraController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    public GameObject player;
+    private Vector3 offset;
+    public float minX;
+    public float maxX;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+      offset = transform.position - player.transform.position;
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+      Vector3 setPosition = transform.position;
+          setPosition.x = player.transform.position.x + offset.x;
+          setPosition.z = player.transform.position.z + offset.z;
+          transform.position = setPosition;
+    }
+}
