c609e12a49af9ddfa7c019a9970341ac2c4bcd92
Martin Kovac <20kovmar@hawken.edu>
2020-03-02 10:40:10 -0500

    Added music, white attack, and basic HP bar on virus, still kinda buggy

---
+++ Game Jam/Assets/Scripts/Attack.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Attack : MonoBehaviour
+{
+    private float attackTime;
+    public float startAttackTime;
+    public Transform attackPos;
+    public float attackRange;
+    public LayerMask enemyLayer;
+    public int damage;
+    public Animator animator;
+    public LayerMask groundLayer;
+
+    private bool attacking = false;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (attackTime <= 0)
+        {
+            if (Input.GetKeyDown(KeyCode.X) && !attacking)
+            {
+                animator.SetTrigger("isAttacking");
+                Collider2D[] enemiesToDamage = Physics2D.OverlapCircleAll(attackPos.position, attackRange, enemyLayer);
+                for (int i = 0; i < enemiesToDamage.Length; i++)
+                {
+                    enemiesToDamage[i].GetComponent<Enemy>().TakeDamage(damage);
+                }
+                attackTime = startAttackTime;
+            }
+        }
+        else
+        {
+            attackTime -= Time.deltaTime;
+            animator.ResetTrigger("isAttacking");
+        }
+    }
+
+    void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(attackPos.position, attackRange);
+    }
+}
---
+++ Game Jam/Assets/Scripts/Enemy.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Enemy : MonoBehaviour
+{
+    public float currentHealth;
+    public float maxHealth;
+    public GameObject obj;
+    public Slider healthBar;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    public void TakeDamage (int Damage){
+      currentHealth -= Damage;
+      if(currentHealth <= 0){
+        Destroy(healthBar);
+        Die();
+      }
+    }
+
+    float CalculateHealth(){
+      return currentHealth / maxHealth;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+      healthBar.value = CalculateHealth();
+    }
+
+    public void Die (){
+      Destroy(gameObject);
+      Destroy(obj);
+    }
+}
